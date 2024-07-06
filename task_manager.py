405d541625b1e2a6e618a42ce92806848a044c83
Amahamed <ashwaqmahamed1@gmail.com>
2024-07-07 00:01:50 +0200

    add and display implementated

--- task_manager.py
+++ task_manager.py
@@ -1,13 +1,70 @@
 import argparse
 import sys
+import csv
+import random
+from tabulate import tabulate
+
+def save_data(task_id,task_title,task_description,priority,category):
+    with open("tasks_info.csv","a") as file:
+        data_writer = csv.writer(file)
+        data_writer.writerow([task_id,task_title,task_description,priority,category,"Incomplete"])
+
+    print("Task successfully added! ")
+
+def unique_ids():
+    id_list = []
+    with open("tasks_info.csv","r") as files:
+        data_writer = csv.reader(files)    
+        for line in data_writer:
+            id_list.append(line[0])
+    
+    return id_list
+
+
+def create_unique_id():
+    id_list = unique_ids()
+    while True:
+        i = random.randrange(0,1000)
+        if len(id_list) == 0:
+            return i
+        elif not str(i) in id_list:
+            return i
+
+
+
+
+def remove_unique_id(id):
+    id_collection = set()
+    id_collection = unique_ids()
+    for i in id_collection:
+        if i == id:
+            id_collection.remove(i)
+            break
+        
+
 
 def add_task():
-    # Implement logic to add a new task
-    print("Testing to see connection")
+    task_title = input("Enter Task Title: ")
+    task_description = input("Enter description: ")
+    priority = input("Enter Priority level (low/medium/high): ")
+    category = input("Enter Category (General/personal/work/other): ")
+    task_id = create_unique_id()
+    save_data(task_id,task_title,task_description,priority,category)
+
 
 def display_tasks():
-    # Implement logic to display tasks
-    pass
+    all_data = [["Task id","Task title","Priority","Category","Status"]]
+    with open("tasks_info.csv","r") as files:
+        data_writer = csv.reader(files)    
+        
+        for line in data_writer:
+            if len(line) >0:
+                display = [line[0],line[1],line[3],line[4],line[5]]
+                all_data.append(display)
+
+                print(tabulate(all_data,headers="firstrow",tablefmt="fancy_grid"))
+            else:
+                print("Currently no tasks!")
 
 def complete_task(task_id):
     # Implement logic to mark a task as completed
@@ -20,7 +77,7 @@ Command-Line Task Manager
 In the fast-paced world we live in, staying organised 
 and on top of your tasks is essential for success. 
 Introducing command-line Task Manager , your ultimate 
-companion in conquering the chaos of daily life. 
+companion in conquering the chaos of daily life for developers. 
           
 valid command: python{version} task_manager.py [option]
 
@@ -33,9 +90,9 @@ options:
 """)
 
 def main():
+    
     version = sys.version_info.major
 
-    
     parser = argparse.ArgumentParser(description="Command-Line Task Manager")
     parser.add_argument("--add", help="Add a new task", action="store_true")
     parser.add_argument("--display", help="Display tasks", action="store_true")
